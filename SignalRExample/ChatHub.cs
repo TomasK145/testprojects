71a7c8a3c24eb15229bd939f515c865fb178790d
tomas.krchnak <tkrchnak@millennium.sk>
2017-12-11 20:44:45 +0100

    signalR example done

---
+++ SignalRExample/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalRExample
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {        
+            //https://docs.microsoft.com/en-us/aspnet/signalr/overview/getting-started/tutorial-getting-started-with-signalr
+            Clients.All.broadcastMessage(name, message);
+        }
+    }
+}
